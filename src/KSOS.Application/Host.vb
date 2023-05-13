4a7ee8544ab2b42d31a69ee2351c5e3a5adb472b
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-05-13 17:28:52 -0500

    a title screen

---
+++ src/KSOS.Application/Host.vb
@@ -0,0 +1,11 @@
+Public Class Host
+    Const OkText = "Ok"
+    Public Sub Run()
+        AnsiConsole.Clear()
+        Dim figlet As New FigletText("Key Seeker of SPLORR!!") With {.Color = Color.Fuchsia, .Justification = Justify.Center}
+        AnsiConsole.Write(figlet)
+        Dim prompt As New SelectionPrompt(Of String) With {.Title = ""}
+        prompt.AddChoice(OkText)
+        AnsiConsole.Prompt(prompt)
+    End Sub
+End Class
---
+++ src/KeySeekerOfSplorr/Program.vb
@@ -0,0 +1,6 @@
+Module Program
+    Sub Main(args As String())
+        Dim host = New Host
+        host.Run()
+    End Sub
+End Module
