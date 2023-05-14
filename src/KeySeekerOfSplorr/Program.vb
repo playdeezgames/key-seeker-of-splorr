f43fe37b76b489d5fd4c15194664a0f1901b27b1
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-05-14 03:55:00 -0500

    screen is 120x30

--- src/KeySeekerOfSplorr/Program.vb
+++ src/KeySeekerOfSplorr/Program.vb
@@ -1,5 +1,12 @@
 Module Program
     Sub Main(args As String())
+        Console.Title = "Key Seeker of SPLORR!!"
+#Disable Warning CA1416 ' Validate platform compatibility
+        Console.BufferWidth = 120
+        Console.BufferHeight = 30
+        Console.WindowWidth = 120
+        Console.WindowHeight = 30
+#Enable Warning CA1416 ' Validate platform compatibility
         Host.Run()
     End Sub
 End Module
