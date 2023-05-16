a840ae85ddd98d47cb6fe7cac2b05c9f932edd1e
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-05-16 04:13:14 -0500

    game over is completed

--- src/KSOS.Application/GameOver.vb
+++ src/KSOS.Application/GameOver.vb
@@ -2,6 +2,7 @@
     Friend Function Run() As Boolean
         AnsiConsole.Clear()
         AnsiConsole.MarkupLine("[red]Yer Dead![/]")
+        World = Nothing
         OkPrompt()
         Return False
     End Function
