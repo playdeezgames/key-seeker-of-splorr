6a57921d363414f0a69c89fa9052091e0d703b53
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-05-17 05:18:10 -0500

    switching inventory detail to a nevermind

--- src/KSOS.Application/InPlay/Items/InventoryDetail.vb
+++ src/KSOS.Application/InPlay/Items/InventoryDetail.vb
@@ -1,7 +1,14 @@
 ﻿Friend Module InventoryDetail
     Friend Sub Run(item As IItem)
         AnsiConsole.Clear()
-        AnsiConsole.MarkupLine($"{item.Name}(x{item.Quantity})")
-        OkPrompt()
+        Dim prompt As New SelectionPrompt(Of String) With {.Title = $"[olive]{item.Name}(x{item.Quantity})[/]"}
+        prompt.AddChoice(NeverMindText)
+        Dim answer = AnsiConsole.Prompt(prompt)
+        Select Case answer
+            Case NeverMindText
+                Return
+            Case Else
+                Throw New NotImplementedException
+        End Select
     End Sub
 End Module
